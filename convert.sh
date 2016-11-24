4835fb621d89f4329a872ef21a051ee2428e29e2
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2016-11-24 16:07:27 -0700

    resize all icons

--- convert.sh
+++ convert.sh
@@ -11,5 +11,6 @@ cp $SRC apple-touch-icon-precomposed.png
 cp $SRC apple-touch-icon.png
 
 for s in 114 120 144 152 57 60 72 76; do
-	echo convert -resize $sx$s $SRC apple-touch-icon-$sx$s.png
+	echo "create $1 icon ${s}x${s}"
+	convert -resize ${s}x${s} $SRC apple-touch-icon-${s}x${s}.png
 done
